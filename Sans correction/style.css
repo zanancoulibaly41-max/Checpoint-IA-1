d4a98d84a28d76dfce334a4e23d8c30e389ae7fa
zanancoulibaly41-max <zanancoulibaly41@gmail.com>
2026-06-03 12:19:20 +0100

    Add files via upload

---
+++ Sans correction/style.css
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.pricing {
+width: 300px;
+margin: auto;
+background-color: #fff;
+box-shdow: 0 0 10px #ccc;
+padding: 10px;
+text-align: left;
+}
+
+.title {
+font-size: 22px;
+font-weight: bold;
+}
+
+.price {
+font-size: 18px;
+color: green;
+}
+
+.features {
+list-style: none;
+padding-left: 0;
+}
+
+.features li {
+padding: 4px;
+border-bottom: 1px solid #eee;
+}
+
+.btn {
+background: blue;
+color: white;
+padding: 10px 20px;
+border: none;
+margin-top: 10px;
+}
+
+.btn:hover {
+background: darkblue;
+}
+</style>
+</head>
+<body>
+
+<div class="pricing">
+<h2 class="title">Basic Plan<h2>
+<p class="price">$9.99 /month</p>
+
+<ul class="features">
+<li>1 GB Storage</li>
+<li>Basic Support</li>
+<li>All Core Features</li>
+</ul>
+
+<button class="btn">Start Trial</button>
+</div>
+
+</body>
+</html>
